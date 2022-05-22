170ecd27e3f89e692195a91b66e030e6df98cfc2
waiterorg <iwaiterorg@gmail.com>
2022-05-22 17:47:28 +0430

    index view for game

--- core/game/views.py
+++ core/game/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def index(request):
+    if request.method == "POST":
+        room_code = request.POST.get("room_code")
+        char_choice = request.POST.get("character_choice")
+        return redirect(
+            '/play/%s?&choice=%s' 
+            %(room_code, char_choice)
+        )
+    return render(request, "index.html", {})
