0b78322d40ee55f620dc89c4b5876f2ec2f1d2aa
waiterorg <iwaiterorg@gmail.com>
2022-05-23 23:18:26 +0430

    Add url for game page

--- core/urls.py
+++ core/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.game.views import index
+from core.game.views import index, game
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('play/<room_code>', game),
 ]
