a44ed0fe2f4628c71b77df604b9ecbaea26029a7
waiterorg <iwaiterorg@gmail.com>
2022-05-24 22:23:58 +0430

    Add WebSocket to game - connect method

---
+++ core/game/consumers.py
@@ -0,0 +1,14 @@
+import json
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
+
+class TicTacToeConsumer(AsyncJsonWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route'\]['kwargs']['room_code']
+        self.room_group_name = 'room_%s' % self.room_name
+
+        # Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        await self.accept()
