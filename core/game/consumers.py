02af594b537701a9a9cd26fa500775fcab948cc5
waiterorg <iwaiterorg@gmail.com>
2022-05-27 18:24:22 +0430

    complete websocket for game

--- core/game/consumers.py
+++ core/game/consumers.py
@@ -20,3 +20,42 @@ class TicTacToeConsumer(AsyncJsonWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
+
+    async def receive(self, text_data):
+        """
+        Receive message from WebSocket.
+        Get the event and send the appropriate event
+        """
+        response = json.loads(text_data)
+        event = response.get("event", None)
+        message = response.get("message", None)
+        if event == 'MOVE':
+            # Send message to room group
+            await self.channel_layer.group_send(self.room_group_name, {
+                'type': 'send_message',
+                'message': message,
+                "event": "MOVE"
+            })
+
+        if event == 'START':
+            # Send message to room group
+            await self.channel_layer.group_send(self.room_group_name, {
+                'type': 'send_message',
+                'message': message,
+                'event': "START"
+            })
+
+        if event == 'END':
+            # Send message to room group
+            await self.channel_layer.group_send(self.room_group_name, {
+                'type': 'send_message',
+                'message': message,
+                'event': "END"
+            })
+
+    async def send_message(self, res):
+        """ Receive message from room group """
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            "payload": res,
+        }))
